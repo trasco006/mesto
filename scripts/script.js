c7eacdecea44fc4b8a4cf281204b8f3c9453b8e8
Kirill Sychevnik <k.sychevnik@yandex.ru>
2020-08-09 12:58:06 +0300

    Не сделана только адаптивность

---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+let editButton = document.querySelector('.profile__edit-button');
+let form = document.querySelector('form')
+let popup = document.querySelector('.popup');
+let escapeButton = document.querySelector('.popup__escape-button');
+let saveButton = document.querySelector('.popup__save-button');
+
+let profileName = document.querySelector('.profile__user-name');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+
+let inputName = document.querySelector('.popup__name');
+let inputSubtitle = document.querySelector('.popup__subtitle');
+
+function popupOpened() {
+  popup.classList.toggle('popup__opened');
+}
+
+inputName.value = profileName.textContent;
+inputSubtitle.value = profileSubtitle.textContent;
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+
+  profileName.textContent = inputName.value;
+  profileSubtitle.textContent = inputSubtitle.value;
+
+  popupOpened();
+}
+
+editButton.addEventListener('click', popupOpened);
+escapeButton.addEventListener('click', popupOpened);
+form.addEventListener('submit', formSubmitHandler);
